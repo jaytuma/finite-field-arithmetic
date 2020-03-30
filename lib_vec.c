001a08329ac0a7e2994d51abc089131770218356
Emanuele Giunta <emanuele.giunta@simail.it>
2020-03-30 18:59:00 +0200

    included *_append, *_get for the data structure and *_string in order to represent vectors

--- lib_vec.c
+++ lib_vec.c
@@ -1,30 +1,39 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <gmp.h>
+
+#define ERROR "\n\t\e[1;31mError\e[0m:"
 
 // - - vector data structure - - //
-/* everything is implicilty a pointer */
+/* holds an dynamically allocated vector of mpz
 
-typedef struct __vec_t
+/* the data structure need to be initialized through a pointer
+ * from that moment on the vector is a pointer to _vec_t and 
+ * values can be modified without passing a pointer.
+ */
+
+typedef struct _vec_mpz_t
 {
 	int len;
 	int size;
-	int* data;
-} _vec_t, *vec_t;
+	mpz_t* data;
+} *vec_mpz_t;
 
-void vec_init(vec_t* vec_pt)
+void vec_mpz_init(vec_mpz_t* vec_pt)
 {
 	*vec_pt = malloc(sizeof(**vec_pt));
-	vec_t vec = *vec_pt;
+	vec_mpz_t vec = *vec_pt;
 	
 	//parameter allocation
-	vec -> data = malloc( 2*sizeof(int) );
+	vec -> data = malloc( 2*sizeof(mpz_t) );
 	vec -> len = 0;
 	vec -> size = 2;
 }
 
-void vec_clear(vec_t* vec_pt)
+void vec_mpz_clear(vec_mpz_t* vec_pt)
 {
-	vec_t vec = *vec_pt;
+	vec_mpz_t vec = *vec_pt;
 	
 	//parameter deallocation
 	free(vec -> data);
@@ -33,11 +42,117 @@ void vec_clear(vec_t* vec_pt)
 	free(vec);
 }
 
+void _vec_mpz_increase(vec_mpz_t vec)
+{
+	//reallocate more memory if needed
+	if(vec -> len == vec -> size)
+	{
+		vec -> size *= 2;
+		vec -> data = realloc( vec -> data, (vec -> size) * sizeof(mpz_t) );		
+	}
+}
+
+void _vec_mpz_decrease(vec_mpz_t vec)
+{
+	//deallocate memory once not needed anymore
+	if(vec -> len < (vec -> size)/2)
+	{
+		vec -> size /= 2;
+		vec -> data = realloc( vec -> data, vec -> size * sizeof(mpz_t));
+	}
+}
+	
+void vec_mpz_append(vec_mpz_t vec, mpz_t num)
+{
+	//check to verify that there is enough space
+	_vec_mpz_increase(vec);
+
+	mpz_init(vec -> data[vec -> len]);
+	mpz_set(vec -> data[vec -> len], num);
+	vec -> len ++;
+}
+
+void vec_mpz_get(mpz_t out, vec_mpz_t vec, int i)
+{
+	//TODO - this code should only be executed in debug mode
+	//sanity check
+	if(i < 0)
+	{
+		fprintf(stderr, "%s, accessing vec_mpz with negative index!", ERROR );	
+	}
+	
+	if(i >= vec -> len)
+	{
+		fprintf(stderr, "%s, accessing vec_mpz out of boundary!", ERROR );	
+	}
+	
+	mpz_set( out, (vec -> data)[i] );
+}
+
+// - - - debug use only - - -
+
+void vec_mpz_string(char** out, vec_mpz_t vec)
+{
+	int i;
+	char str[1024];
+	char buff[4096];
+	mpz_t tmp;
+	mpz_init(tmp);	
+		
+	*out = "[";
+	
+	strcat(buff, "[");
+	
+	
+	if(vec -> len > 0)
+	{
+		//first element
+		vec_mpz_get(tmp, vec, i);
+		gmp_sprintf(str, "%Zd", tmp);
+		strcat(buff, str);
+		
+		
+		for(i = 1; i < (vec -> len); i++)
+		{
+			vec_mpz_get(tmp, vec, i);
+			gmp_sprintf(str, ", %Zd", tmp);
+			strcat(buff, str);
+		}
+	}
+	
+	strcat(buff, "]");
+	mpz_clear(tmp);
+	*out = buff;
+}
+
 int main()
 {
-	vec_t vec;
-	vec_init(&vec);
-	vec_clear(&vec);
+	printf("ok\n");
 	
-	printf("all ok man! \n");
+	vec_mpz_t vec;
+	vec_mpz_init(&vec);
+	
+	mpz_t tmp;
+	mpz_init(tmp);
+	
+	/*
+	mpz_set_ui(tmp, 1);	
+
+	vec_mpz_append(vec, tmp);
+	
+	mpz_set_ui(tmp, 65);
+	vec_mpz_append(vec, tmp);
+	
+	mpz_set_ui(tmp, 59);
+	vec_mpz_append(vec, tmp);
+	
+	printf("fin qui ok\n");
+	fflush(stdout);
+	
+	*/
+	char* str;
+	vec_mpz_string(&str, vec);
+	printf("vec = %s\n", str);
+	
+	//vec_mpz_clear(&vec);
 }
